474aaf6b0f1d803ba88ea136d38b9770a7dda27d
navinrcs <navinkhagaria.nk@gmail.com>
2023-09-21 09:25:28 +0530

    Initial commit

---
+++ jenkins.java
@@ -0,0 +1,11 @@
+public class jenkins{
+
+    public static void main(String[] args) {
+
+        System.out.println("i am navin");
+        System.out.println("All is done");
+        System.out.println("working fine");
+        
+        
+    }
+}
